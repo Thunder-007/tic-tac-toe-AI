56e5fe2ea4f575e1fe26550ee2b176bb5ec78035
harsha <v.harshavardhan3@gmail.com>
2023-02-15 00:48:08 +0530

    static code players and some refactor

--- p5script.js
+++ p5script.js
@@ -3,26 +3,24 @@ let board = [
     ['', '', ''],
     ['', '', '']
 ];
-let available_spots = [];
-let players = ['X', 'O'];
-let curr_player_index = Math.round(Math.random());
+let ai = 'X';
+let human = 'O';
+let curr_player = human;
 let result_message = document.getElementById("result_message");
 let width = 300;
 let height = 300;
+let one_third_width = width / 3;
+let one_third_height = height / 3;
 
 function setup() {
     canvas = createCanvas(width, height);
     canvas.parent('p5canvas');
     frameRate(10);
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            available_spots.push([row, col]);
-        }
-    }
 }
 
 function checkWinner() {
     let winner = null;
+    let open_spot_exists = false;
     for (let index = 0; index < 3; index++) {
         if (board[index][0] === board[index][1] && board[index][1] === board[index][2] && board[index][0] !== '') {
             winner = board[index][0];
@@ -35,55 +33,55 @@ function checkWinner() {
     } else if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[1][1] !== '') {
         winner = board[0][2];
     }
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (board[row][col] === '') {
+                open_spot_exists = true;
+            }
+        }
+    }
+    if (winner == null && !open_spot_exists) {
+        return 'tie';
+    }
     return winner;
 }
 
+
 function nextTurn() {
-    if (available_spots.length) {
-        let random_index = floor(random(available_spots.length));
-        let curr_spot = available_spots.splice(random_index, 1)[0];
-        let curr_x = curr_spot[0];
-        let curr_y = curr_spot[1];
-        board[curr_x][curr_y] = players[curr_player_index];
-        curr_player_index ^= 1;
-    } else {
+    let available_spots = [];
+    for(let row = 0 ; row < 3 ; row++) {
+        for(let col = 0 ; col < 3 ; col++) {
+            if(board[row][col] == '') {
+                available_spots.push([row, col]);
+            }
+        }
+    }
+    if(available_spots.length == 0) {
         return false;
     }
+    let random_index = floor(random(available_spots.length));
+    let random_spot = available_spots[random_index];
+    let row = random_spot[0];
+    let col = random_spot[1];
+    board[row][col] = ai;
     return true;
 }
 
 function mousePressed() {
-    let one_by_three_width = width / 3;
-    let one_by_three_height = height / 3;
-    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height && available_spots.length !== 0) {
-        col = floor(mouseX / one_by_three_width);
-        row = floor(mouseY / one_by_three_height);
-        if(board[row][col] === '') {
-            board[row][col] = players[curr_player_index];
-            available_spots = available_spots.filter(function (spot) {
-                return spot[0] !== row || spot[1] !== col
-            });
-            curr_player_index ^= 1;
-            winner = checkWinner();
-            if (winner == null) {
+    if (curr_player == human) {
+        let row = floor(mouseY / one_third_height);
+        let col = floor(mouseX / one_third_width);
+        if(row >= 0 && row < 3 && col >= 0 && col < 3)
+            if (board[row][col] == '' && curr_player == human ) {
+                board[row][col] = human;
+                currentPlayer = ai;
                 nextTurn();
             }
-        }
     }
 }
 
 function draw() {
-    winner = checkWinner();
-    if (winner != null) {
-        result = "Winner is " + winner;
-        result_message.innerText = result;
-        noLoop();
-    }
-    else if(available_spots.length === 0) {
-        result = "Draw";
-        result_message.innerText = result;
-        noLoop();
-    }
+    background(255);
     let one_by_three_width = width / 3;
     let one_by_three_height = height / 3;
     line(0, 0, 0, height);
@@ -109,6 +107,19 @@ function draw() {
                 line(start_x, start_y, start_x + width / 6, start_y + height / 6);
                 line(start_x, start_y + height / 6, start_x + width / 6, start_y);
             }
+        }
+    }
+    let result = checkWinner();
+    if (result != null) {
+        noLoop();
+        if (result == 'tie') {
+            result_message.innerHTML = "It's a tie!";
+        }
+        else if (result == human) {
+            result_message.innerHTML = `You won!`;
+        }
+        else {
+            result_message.innerHTML = `AI won!`;
         }
     }
 }
